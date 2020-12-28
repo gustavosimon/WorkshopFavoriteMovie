f801a98facc549b1a2142d590c22b482c54df593
gustavosimon <gustavo.andre.simon@hotmail.com>
2020-12-27 23:32:09 -0300

    Created functions to verify if the movie is already favorited and a function to save the movie as a favorite if this not favorited yet

--- app/src/main/java/com/jessica/yourfavoritemovies/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/jessica/yourfavoritemovies/home/viewmodel/HomeViewModel.kt
@@ -4,7 +4,10 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.database.*
+import com.jessica.yourfavoritemovies.Constants
 import com.jessica.yourfavoritemovies.MovieRepository
+import com.jessica.yourfavoritemovies.MovieUtil
 import com.jessica.yourfavoritemovies.model.Result
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,6 +18,7 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
     var stateList: MutableLiveData<List<Result>> = MutableLiveData()
     var error: MutableLiveData<String> = MutableLiveData()
     var loading: MutableLiveData<Boolean> = MutableLiveData()
+    var stateFavorite: MutableLiveData<Result> = MutableLiveData()
 
     fun getListMovies(language: String) {
         viewModelScope.launch {
@@ -33,11 +37,52 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
-    //TODO - Implementar a função de savar o filme favorito
+    fun saveFavorite(result: Result) {
+        val database = FirebaseDatabase.getInstance()
+        val reference = database.getReference(MovieUtil.getUserId(getApplication()).toString() + Constants.FAVORITES_PATH)
+        reference.orderByKey().addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var movieAdded = false
+
+                for (resultSnapshot in dataSnapshot.children) {
+                    val firebaseResult = resultSnapshot.getValue(Result::class.java)
+
+                    when {
+                        firebaseResult?.id != null && firebaseResult.id == result.id -> {
+                            var movieAdded = true
+                        }
+                    }
+                }
+                when {
+                    movieAdded -> {
+                        errorMessage("The movie has already been added to favorites!")
+                    }
+                    else -> {
+                        saveMovieFavorited(reference, result)
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    fun saveMovieFavorited(reference: DatabaseReference, result: Result) {
+        val key = reference.push().key
+        key?.let {
+            reference.child(it).setValue(result)
+            reference.child(key).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val movieResult = snapshot.getValue(Result::class.java)
+                    stateFavorite.value = movieResult
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    errorMessage(error.message)
+                }
+            })
+        }
+    }
 
     private fun errorMessage(message: String) {
         error.value = message
     }
 }
-
-
